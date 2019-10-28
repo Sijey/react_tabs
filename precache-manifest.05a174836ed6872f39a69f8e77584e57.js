8140364dc0c63026ba7ce3a527d469bd310dcbac
Sijey <Popov040391@gmail.com>
2019-10-28 16:05:24 +0200

    Updates

---
+++ precache-manifest.05a174836ed6872f39a69f8e77584e57.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cfdedd7966653e73350210bd427aba1",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "32d6b96a366773a6b59b",
+    "url": "/react_tabs/static/css/main.65938976.chunk.css"
+  },
+  {
+    "revision": "ec14eb012e5d855c520e",
+    "url": "/react_tabs/static/js/2.9fbf4d89.chunk.js"
+  },
+  {
+    "revision": "32d6b96a366773a6b59b",
+    "url": "/react_tabs/static/js/main.d61db230.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
